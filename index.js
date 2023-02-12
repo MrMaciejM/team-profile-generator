0cf61eef898b9b371ca9f103572d488fa21fa6fa
maciej <63735702+MrMaciejM@users.noreply.github.com>
2023-02-12 17:43:08 +0000

    trying to fix issue with multiple prompts not working

--- index.js
+++ index.js
@@ -15,35 +15,131 @@ const questions = require("./questions");
 // TODO: Write Code to gather information about the development team members, and render the HTML file.
 let result = [];
 
+const addPersonnel = {
+  type: "list",
+  name: "addEmployee",
+  message: "Choose one of the following:",
+  choices: ["Add an engineer", "Add an intern", "Finish building the team"],
+};
+
 function init() {
-  inquirer.prompt(mgrQuestions).then((mgrData) => {
-    const newManager = new Manager(
-      mgrData.name,
-      mgrData.id,
-      mgrData.email,
-      mgrData.officeNumber
-    );
-    const createHtml = render(result);
+  //console.log(mgrQuestions[4]);
+  return inquirer.prompt(mgrQuestions).then((mgrData) => {
+    // prettier-ignore
+    const newManager = new Manager(mgrData.name,mgrData.id,mgrData.email,mgrData.officeNumber);
     const addEng = "Add an engineer";
     const addInt = "Add an intern";
     const finish = "Finish building the team";
 
-    if (mgrData.choices === addEng) {
-      inquirer.prompt(engQuestions).then((engData) => {
-        result.push(engData);
-        fs.writeFile("team.html", createHtml, (error) => {
-          error ? console.log(error) : "team created";
-        });
+    if (mgrData.addEmployee === finish) {
+      result.push(newEngineer);
+      fs.writeFile("team.html", render(result), (error) => {
+        error ? console.log(error) : "team created";
       });
     }
 
-    //inquirer.prompt(choices).then((employeeData) => {
-    //
-    //console.log(employeeData);
-    //});
-  });
-}
+    function addMoreEmployees() {
+      return inquirer.prompt(addPersonnel).then((data) => {
+        //
+        //console.log(data);
+        if (data.addEmployee == addEng) {
+          //
+          return inquirer.prompt(engQuestions).then((engData) => {
+            // prettier-ignore
+            const newEngineer = new Engineer(engData.name,engData.id,engData.email,engData.github);
+
+            result.push(newEngineer);
+            fs.writeFile("team.html", render(result), (error) => {
+              error ? console.log(error) : "team created";
+            });
+            return data;
+          });
+        } else {
+          return addMoreEmployees();
+        }
+      });
+    }
+    addMoreEmployees();
+
+    //console.log(mgrData);
+    //console.log(mgrData.addEmployee);
+  }); // end of then(data)
+  //
+} // end of init()
 init();
+
+//prompt to add another employee
+// inquirer.prompt(addPersonnel).then((data) => {
+//   //console.log();
+//   function askUserRepeatedly() {
+//     if (data.addEmployee === addEng) {
+//       // return this promise...
+//       return inquirer.prompt(engQuestions).then((engData) => {
+//         // ...
+//         // prettier-ignore
+//         const newEngineer = new Engineer(engData.name,engData.id,engData.email,engData.github);
+//         result.push(newEngineer);
+//         fs.writeFile("team.html", render(result), (error) => {
+//           error ? console.log(error) : "team created";
+//         });
+//       });
+//     }
+//   }
+
+//   // ...so it can be fed into the parent chain
+//   return askUserRepeatedly();
+
+// Add Engineer
+//   if (data.addEmployee === addEng) {
+//     inquirer.prompt(engQuestions).then((engData) => {
+//       // prettier-ignore
+//       const newEngineer = new Engineer(engData.name,engData.id,engData.email,engData.github);
+//       result.push(newEngineer);
+//       fs.writeFile("team.html", render(result), (error) => {
+//         error ? console.log(error) : "team created";
+//       });
+//       //
+//     }); // end of engQuestions promise
+//   } // end of it statement
+//
+
+//=============================
+//console.log(addPersonnel[1]);
+
+// if (mgrData.addEmployee == finish) {
+//   result.push(newManager);
+//   fs.writeFile("team.html", render(result), (error) => {
+//     error ? console.log(error) : "team created";
+//   });
+// }
+
+// function to loop and add employees
+// let addMore = true;
+// function addMoreEmplyees() {
+
+// }
+
+// if (mgrData.addEmployee === addEng) {
+//   for (let i = 0; i < 1; ) {
+//     inquirer.prompt(engQuestions).then((engData) => {
+//       // prettier-ignore
+//       const newEngineer = new Engineer(engData.name,engData.id,engData.email,engData.github);
+
+//       result.push(newManager, newEngineer);
+//       fs.writeFile("team.html", render(result), (error) => {
+//         error ? console.log(error) : "team created";
+//       });
+
+//       // set to ask question again
+//       //
+//     }); // inquirer.then close function
+//   }
+// }
+
+//
+///});
+//}
+//init();
 
 /*
 //NOTE: mgrData must be pushed into an array (which is result), because of the map in page-template.js file. Otherwise you will get TypeError.
@@ -55,3 +151,5 @@ init();
     });
 
 */
+
+// =================
--- questions.js
+++ questions.js
@@ -23,18 +23,49 @@ const questions = [
       name: "officeNumber",
       message: "Enter team manager's number",
     },
-    {
-      type: "list",
-      name: "addEmployee",
-      message: "Choose one of the following:",
-      choices: ["Add an engineer", "Add an intern", "Finish building the team"],
-    },
   ]),
   (engQuestions = [
     {
       type: "input",
       name: "name",
       message: "Enter Engineer's name",
+    },
+    {
+      type: "input",
+      name: "id",
+      message: "Enter Engineer's id",
+    },
+    {
+      type: "input",
+      name: "email",
+      message: "Enter Engineer's email address",
+    },
+    {
+      type: "input",
+      name: "github",
+      message: "Enter Engineer's github",
+    },
+  ]),
+  (internQuestions = [
+    {
+      type: "input",
+      name: "name",
+      message: "Enter intern's name",
+    },
+    {
+      type: "input",
+      name: "id",
+      message: "Enter intern's id",
+    },
+    {
+      type: "input",
+      name: "email",
+      message: "Enter intern's email address",
+    },
+    {
+      type: "input",
+      name: "school",
+      message: "Enter intern's school name",
     },
   ]),
 ];
